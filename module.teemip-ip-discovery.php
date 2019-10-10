631fd24633d64e688ced8d74ea4997bcdfd5573f
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2019-10-10 12:12:49 +0200

    Evolutions for 0.4.0

--- module.teemip-ip-discovery.php
+++ module.teemip-ip-discovery.php
@@ -1,7 +1,7 @@
 <?php
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-ip-discovery/0.3.0',
+	'teemip-ip-discovery/0.4.0',
 	array(
 		// Identification
 		//
