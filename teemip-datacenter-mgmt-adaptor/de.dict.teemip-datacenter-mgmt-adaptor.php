f60c39947979048a2565782bf18a5bc6ae47e63b
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2020-01-23 18:46:52 +0100

    Redistribute files to facilitate product build #2

---
+++ teemip-datacenter-mgmt-adaptor/de.dict.teemip-datacenter-mgmt-adaptor.php
@@ -0,0 +1,78 @@
+<?php
+// Copyright (C) 2014 TeemIp
+//
+//   This file is part of TeemIp.
+//
+//   TeemIp is free software; you can redistribute it and/or modify	
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   TeemIp is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with TeemIp. If not, see <http://www.gnu.org/licenses/>
+
+/**
+ * @copyright   Copyright (C) 2014 TeemIp
+ * @copyright   Copyright (C) 2014 ITOMIG GmbH (deutsche Übersetzung)
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// Class: Rack
+//
+ 
+Dict::Add('DE DE', 'English', 'English', array(
+	'Class:Rack/Attribute:ipaddress_id' => 'IP Adresse',
+	'Class:Rack/Attribute:ipaddress_id+' => '',
+	'Class:Rack/Attribute:ipaddress_name' => 'IP Adressname',
+	'Class:Rack/Attribute:ipaddress_name+' => '',
+));
+
+//
+// Class: Enclosure
+//
+
+Dict::Add('DE DE', 'English', 'English', array(
+	'Class:Enclosure/Attribute:ipaddress_id' => 'IP Addresse',
+	'Class:Enclosure/Attribute:ipaddress_id+' => '',
+	'Class:Enclosure/Attribute:ipaddress_name' => 'IP Adressname',
+	'Class:Enclosure/Attribute:ipaddress_name+' => '',
+));
+
+//
+// Class: PowerConnection
+//
+
+Dict::Add('DE DE', 'English', 'English', array(
+	'Class:PowerConnection/Attribute:ipaddress_id' => 'IP Addresse',
+	'Class:PowerConnection/Attribute:ipaddress_id+' => '',
+	'Class:PowerConnection/Attribute:ipaddress_name' => 'IP Adressname',
+	'Class:PowerConnection/Attribute:ipaddress_name+' => '',
+));
+
+//
+// Class: PowerSource
+//
+
+Dict::Add('DE DE', 'English', 'English', array(
+	'Class:PowerSource/Attribute:ipaddress_id' => 'IP Addresse',
+	'Class:PowerSource/Attribute:ipaddress_id+' => '',
+	'Class:PowerSource/Attribute:ipaddress_name' => 'IP Adressname',
+	'Class:PowerSource/Attribute:ipaddress_name+' => '',
+));
+
+//
+// Class: PDU
+//
+
+Dict::Add('DE DE', 'English', 'English', array(
+	'Class:PDU/Attribute:ipaddress_id' => 'IP Addresse',
+	'Class:PDU/Attribute:ipaddress_id+' => '',
+	'Class:PDU/Attribute:ipaddress_name' => 'IP Adressname',
+	'Class:PDU/Attribute:ipaddress_name+' => '',
+));
