9a03ac5aeaabe506167b3ffd1ce4569be9df3e41
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2020-02-04 12:00:55 +0100

    Centralize plugin in teemip-config-mgmt-adaptor

--- teemip-endusers-devices-adaptor/fr.dict.teemip-endusers-devices-adaptor.php
+++ teemip-endusers-devices-adaptor/fr.dict.teemip-endusers-devices-adaptor.php
@@ -1,9 +1,9 @@
 <?php
-// Copyright (C) 2014 TeemIp
+// Copyright (C) 2020 TeemIp
 //
 //   This file is part of TeemIp.
 //
-//   TeemIp is free software; you can redistribute it and/or modify	
+//   TeemIp is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU Affero General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
@@ -17,7 +17,7 @@
 //   along with TeemIp. If not, see <http://www.gnu.org/licenses/>
 
 /**
- * @copyright   Copyright (C) 2014 TeemIp
+ * @copyright   Copyright (C) 2020 TeemIp
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
