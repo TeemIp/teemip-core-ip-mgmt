85032330b034e46ed1e665f9ca0d65b6127a947f
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2020-03-02 14:11:22 +0100

    Add dependency to config-mgmt-adaptor

--- teemip-storage-mgmt-adaptor/module.teemip-storage-mgmt-adaptor.php
+++ teemip-storage-mgmt-adaptor/module.teemip-storage-mgmt-adaptor.php
@@ -35,10 +35,11 @@ SetupWebPage::AddModule(
 		'dependencies' => array(
 			'itop-storage-mgmt/2.7.0',
 			'teemip-ip-mgmt/2.6.0',
+			'teemip-config-mgmt-adaptor/2.6.0'
 		),
 		'mandatory' => false,
 		'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
- 		'auto_select' => 'SetupInfo::ModuleIsSelected("teemip-ip-mgmt") && SetupInfo::ModuleIsSelected("itop-storage-mgmt")',
+ 		'auto_select' => 'SetupInfo::ModuleIsSelected("itop-storage-mgmt") && SetupInfo::ModuleIsSelected("teemip-ip-mgmt") && SetupInfo::ModuleIsSelected("teemip-config-mgmt-adaptor")',
 
 		// Components
 		//
