3e8bdd6bbf8fe40f21662e9c6cce3a4f4e10e16d
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2020-04-08 15:02:26 +0200

    Update for TeemIp 2.6.0

--- module.teemip-ip-discovery.php
+++ module.teemip-ip-discovery.php
@@ -1,7 +1,7 @@
 <?php
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-ip-discovery/0.4.0',
+	'teemip-ip-discovery/0.5.0',
 	array(
 		// Identification
 		//
@@ -11,7 +11,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'teemip-ip-mgmt/2.5.0',
+			'teemip-ip-mgmt/2.6.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
