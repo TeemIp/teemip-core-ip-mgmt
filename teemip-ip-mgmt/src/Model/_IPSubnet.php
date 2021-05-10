1ed33fa12ff5495e5ace502df751f0b08f856422
xtophe38 <christophe.naud@gmail.com>
2021-05-10 20:29:03 +0200

    First modifications to fit both 2.7 and 3.0 branches.

--- teemip-ip-mgmt/src/Model/_IPSubnet.php
+++ teemip-ip-mgmt/src/Model/_IPSubnet.php
@@ -11,18 +11,16 @@ use IPObject;
 use utils;
 use WebPage;
 
-class _IPSubnet extends IPObject
-{
-	/*
+class _IPSubnet extends IPObject {
+	/**
 	 * Returns size of subnet
 	 *
 	 * @return int
 	 */
-	public function GetSize()
-	{
+	public function GetSize() {
 		return 1;
 	}
-	
+
 	/**
 	 * Return % of occupancy of objects linked to $this
 	 *
@@ -30,11 +28,10 @@ class _IPSubnet extends IPObject
 	 *
 	 * @return int
 	 */
-	public function GetOccupancy($sObject)
-	{
+	public function GetOccupancy($sObject) {
 		return 0;
 	}
-	
+
 	/**
 	 * Return next operation after current one
 	 *
@@ -42,35 +39,48 @@ class _IPSubnet extends IPObject
 	 *
 	 * @return string
 	 */
-	public function GetNextOperation($sOperation)
-	{
-		switch ($sOperation)
-		{
-			case 'findspace': return 'dofindspace';
-			case 'dofindspace': return 'findspace';
-				
-			case 'listips': return 'dolistips';
-			case 'dolistips': return 'listips';
-				
-			case 'shrinksubnet': return 'doshrinksubnet';
-			case 'doshrinksubnet': return 'shrinksubnet';
-				
-			case 'splitsubnet': return 'dosplitsubnet';
-			case 'dosplitsubnet': return 'splitsubnet';
-				
-			case 'expandsubnet': return 'doexpandsubnet';
-			case 'doexpandsubnet': return 'expandsubnet';
-			
-			case 'csvexportips': return 'docsvexportips';
-			case 'docsvexportips': return 'csvexportips';
-	
-			case 'calculator': return 'docalculator';
-			case 'docalculator': return 'calculator';
-	
-			default: return '';
+	public function GetNextOperation($sOperation) {
+		switch ($sOperation) {
+			case 'findspace':
+				return 'dofindspace';
+			case 'dofindspace':
+				return 'findspace';
+
+			case 'listips':
+				return 'dolistips';
+			case 'dolistips':
+				return 'listips';
+
+			case 'shrinksubnet':
+				return 'doshrinksubnet';
+			case 'doshrinksubnet':
+				return 'shrinksubnet';
+
+			case 'splitsubnet':
+				return 'dosplitsubnet';
+			case 'dosplitsubnet':
+				return 'splitsubnet';
+
+			case 'expandsubnet':
+				return 'doexpandsubnet';
+			case 'doexpandsubnet':
+				return 'expandsubnet';
+
+			case 'csvexportips':
+				return 'docsvexportips';
+			case 'docsvexportips':
+				return 'csvexportips';
+
+			case 'calculator':
+				return 'docalculator';
+			case 'docalculator':
+				return 'calculator';
+
+			default:
+				return '';
 		}
 	}
-	
+
 	/**
 	 * Get parameters used for operation
 	 *
@@ -79,11 +89,9 @@ class _IPSubnet extends IPObject
 	 * @return array
 	 * @throws \Exception
 	 */
-	public function GetPostedParam($sOperation)
-	{
+	public function GetPostedParam($sOperation) {
 		$aParam = array();
-		switch ($sOperation)
-		{
+		switch ($sOperation) {
 			case 'dofindspace':
 				$aParam['rangesize'] = utils::ReadPostedParam('rangesize', '', 'raw_data');
 				$aParam['maxoffer'] = utils::ReadPostedParam('maxoffer', 'DEFAULT_MAX_FREE_SPACE_OFFERS', 'raw_data');
@@ -91,8 +99,8 @@ class _IPSubnet extends IPObject
 				$aParam['type'] = '';
 				$aParam['location_id'] = '';
 				$aParam['requestor_id'] = '';
-			break;
-					
+				break;
+
 			case 'dolistips':
 				$aParam['first_ip'] = filter_var(utils::ReadPostedParam('attr_firstip', '', 'raw_data'), FILTER_VALIDATE_IP);
 				$aParam['last_ip'] = filter_var(utils::ReadPostedParam('attr_lastip', '', 'raw_data'), FILTER_VALIDATE_IP);
@@ -101,29 +109,30 @@ class _IPSubnet extends IPObject
 				$aParam['domain_id'] = '';
 				$aParam['usage_id'] = '';
 				$aParam['requestor_id'] = '';
-			break;
-			
+				break;
+
 			case 'doshrinksubnet':
 			case 'dosplitsubnet':
 			case 'doexpandsubnet':
 				$aParam['scale_id'] = utils::ReadPostedParam('scale_id', '', 'raw_data');
 				$aParam['requestor_id'] = utils::ReadPostedParam('attr_requestor_id', null);
-			break;
+				break;
 
 			case 'docsvexportips':
 				$aParam['first_ip'] = filter_var(utils::ReadPostedParam('attr_firstip', '', 'raw_data'), FILTER_VALIDATE_IP);
 				$aParam['last_ip'] = filter_var(utils::ReadPostedParam('attr_lastip', '', 'raw_data'), FILTER_VALIDATE_IP);
-			break;
-			
+				break;
+
 			case 'docalculator':
 				$aParam['ip'] = filter_var(utils::ReadPostedParam('attr_ip', '', 'raw_data'), FILTER_VALIDATE_IP);
 				$aParam['mask'] = filter_var(utils::ReadPostedParam('attr_gatewayip', '', 'raw_data'), FILTER_VALIDATE_IP);
 				$aParam['cidr'] = filter_var(utils::ReadPostedParam('cidr', '', 'raw_data'), FILTER_VALIDATE_INT);
-			break;
-			
+				break;
+
 			default:
 				break;
 		}
+
 		return $aParam;
 	}
 
@@ -136,50 +145,38 @@ class _IPSubnet extends IPObject
 	 * @throws \ArchivedObjectException
 	 * @throws \CoreException
 	 */
-	public function GetAttributeParams($sAttCode)
-	{
+	public function GetAttributeParams($sAttCode) {
 		$aParams = array();
-		if (($sAttCode == 'ip_occupancy') || ($sAttCode == 'range_occupancy'))
-		{
-			if ($sAttCode == 'ip_occupancy')
-			{
+		if (($sAttCode == 'ip_occupancy') || ($sAttCode == 'range_occupancy')) {
+			if ($sAttCode == 'ip_occupancy') {
 				$Occupancy = $this->GetOccupancy('IPAddress');
-			}
-			else
-			{
+			} else {
 				$Occupancy = $this->GetOccupancy('IPRange');
 			}
 			$sOrgId = $this->Get('org_id');
-			if ($sOrgId != null)
-			{
+			if ($sOrgId != null) {
 				$sLowWaterMark = IPConfig::GetFromGlobalIPConfig('subnet_low_watermark', $sOrgId);
 				$sHighWaterMark = IPConfig::GetFromGlobalIPConfig('subnet_high_watermark', $sOrgId);
-				if ($Occupancy >= $sHighWaterMark)
-				{
+				if ($Occupancy >= $sHighWaterMark) {
 					$sColor = RED;
+				} else {
+					if ($Occupancy >= $sLowWaterMark) {
+						$sColor = YELLOW;
+					} else {
+						$sColor = GREEN;
+					}
 				}
-				else if ($Occupancy >= $sLowWaterMark)
-				{
-					$sColor = YELLOW;
-				}
-				else
-				{
-					$sColor = GREEN;
-				}
-				$aParams ['value'] = round ($Occupancy, 0);
+				$aParams ['value'] = round($Occupancy, 0);
 				$aParams ['color'] = $sColor;
-			}
-			else
-			{
+			} else {
 				$aParams ['value'] = 0;
 				$aParams ['color'] = GREEN;
 			}
-		}
-		else
-		{
+		} else {
 			$aParams ['value'] = 0;
 			$aParams ['color'] = GREEN;
 		}
+
 		return ($aParams);
 	}
 
@@ -190,8 +187,7 @@ class _IPSubnet extends IPObject
 	 *
 	 * @return int
 	 */
-	public function IPCount($sStatus)
-	{
+	public function IPCount($sStatus) {
 		return 0;
 	}
 
@@ -202,8 +198,7 @@ class _IPSubnet extends IPObject
 	 *
 	 * @return string
 	 */
-	public function GetFreeIP($iCreationOffset)
-	{
+	public function GetFreeIP($iCreationOffset) {
 		return '';
 	}
 
@@ -212,22 +207,17 @@ class _IPSubnet extends IPObject
 	 * @throws \CoreException
 	 * @throws \CoreUnexpectedValue
 	 */
-	public function OnInsert()
-	{
+	public function OnInsert() {
 		parent::OnInsert();
 
-		if (class_exists('IPDiscovery'))
-		{
-			if ($this->Get('ipdiscovery_ping_enabled') == 'no')
-			{
+		if (class_exists('IPDiscovery')) {
+			if ($this->Get('ipdiscovery_ping_enabled') == 'no') {
 				$this->Set('ping_enabled', 'no');
 			}
-			if ($this->Get('ipdiscovery_iplookup_enabled') == 'no')
-			{
+			if ($this->Get('ipdiscovery_iplookup_enabled') == 'no') {
 				$this->Set('iplookup_enabled', 'no');
 			}
-			if ($this->Get('ipdiscovery_scan_enabled') == 'no')
-			{
+			if ($this->Get('ipdiscovery_scan_enabled') == 'no') {
 				$this->Set('scan_enabled', 'no');
 			}
 		}
@@ -238,22 +228,17 @@ class _IPSubnet extends IPObject
 	 * @throws \CoreException
 	 * @throws \CoreUnexpectedValue
 	 */
-	public function OnUpdate()
-	{
+	public function OnUpdate() {
 		parent::OnUpdate();
 
-		if (class_exists('IPDiscovery'))
-		{
-			if ($this->Get('ipdiscovery_ping_enabled') == 'no')
-			{
+		if (class_exists('IPDiscovery')) {
+			if ($this->Get('ipdiscovery_ping_enabled') == 'no') {
 				$this->Set('ping_enabled', 'no');
 			}
-			if ($this->Get('ipdiscovery_iplookup_enabled') == 'no')
-			{
+			if ($this->Get('ipdiscovery_iplookup_enabled') == 'no') {
 				$this->Set('iplookup_enabled', 'no');
 			}
-			if ($this->Get('ipdiscovery_scan_enabled') == 'no')
-			{
+			if ($this->Get('ipdiscovery_scan_enabled') == 'no') {
 				$this->Set('scan_enabled', 'no');
 			}
 		}
@@ -269,10 +254,8 @@ class _IPSubnet extends IPObject
 	 * @return int
 	 * @throws \CoreException
 	 */
-	public function GetAttributeFlags($sAttCode, &$aReasons = array(), $sTargetState = '')
-	{
-		switch ($sAttCode)
-		{
+	public function GetAttributeFlags($sAttCode, &$aReasons = array(), $sTargetState = '') {
+		switch ($sAttCode) {
 			case 'last_discovery_date':
 			case 'ping_duration':
 			case 'ping_discovered':
@@ -286,6 +269,7 @@ class _IPSubnet extends IPObject
 			default:
 				break;
 		}
+
 		return parent::GetAttributeFlags($sAttCode, $aReasons, $sTargetState);
 	}
 
@@ -298,10 +282,8 @@ class _IPSubnet extends IPObject
 	 * @return int
 	 * @throws \CoreException
 	 */
-	public function GetInitialStateAttributeFlags($sAttCode, &$aReasons = array())
-	{
-		switch ($sAttCode)
-		{
+	public function GetInitialStateAttributeFlags($sAttCode, &$aReasons = array()) {
+		switch ($sAttCode) {
 			case 'last_discovery_date':
 			case 'ping_duration':
 			case 'iplookup_duration':
@@ -311,7 +293,34 @@ class _IPSubnet extends IPObject
 			default:
 				break;
 		}
+
 		return parent::GetInitialStateAttributeFlags($sAttCode, $aReasons);
+	}
+
+	/**
+	 * Display result of IPvi calculator
+	 *
+	 * @param \WebPage $oP
+	 * @param $oAppContext
+	 * @param $aParam
+	 *
+	 * @throws \Exception
+	 */
+	public function DisplayCalculatorOutput(WebPage $oP, $aParam) {
+		$this->DisplayBareTab($oP, 'UI:IPManagement:Action:DoCalculator:');
+		$oP->add($this->GetCalculatorOutput($oP, $aParam));
+	}
+
+	/**
+	 * Get result of IPvi calculator
+	 *
+	 * @param \WebPage $oP
+	 * @param $aParam
+	 *
+	 * @return string
+	 */
+	public function GetCalculatorOutput(WebPage $oP, $aParam) {
+		return '';
 	}
 
 	/**
