e00acf315080414e861320a95021d370d51f75bd
xtophe38 <christophe.naud@gmail.com>
2021-07-23 16:03:05 +0200

    new for TeemIp's Handy Framework

---
+++ teemip-handy-framework/en.dict.teemip-handy-framework.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2021 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+/////////////////////////////////////////////////////////////////////
+// Classes in Teemip's Handy Framework Module'
+//////////////////////////////////////////////////////////////////////
+//
+
+//
+// TeemIp specific attributes
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Core:AttributeIPPercentage' => 'IP percentage',
+	'Core:AttributeIPPercentage+' => 'Graphical display for percentage of usage',
+	'Core:AttributeMacAddress' => 'MAC address',
+	'Core:AttributeMacAddress+' => 'MAC adress string',
+));
