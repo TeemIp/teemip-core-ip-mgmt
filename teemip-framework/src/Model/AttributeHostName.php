c0daa97a7ab7a6c5dcde3901c1e07ae63c4e10e0
xtophe38 <christophe.naud@gmail.com>
2021-11-16 16:46:56 +0100

    Allow validation pattern to be overwritten from config file

--- teemip-framework/src/Model/AttributeHostName.php
+++ teemip-framework/src/Model/AttributeHostName.php
@@ -4,15 +4,22 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
-class AttributeHostName extends AttributeString
-{
-	public function GetValidationPattern()
-	{
+class AttributeHostName extends AttributeString {
+	const MODULE_CODE = 'teemip-ip-mgmt';
+	const FUNCTION_CODE = 'hostname_validation_pattern';
+
+	public function GetValidationPattern() {
 		// By default, pattern matches RFC 1123 plus '_'
 		// Factorize old regex and protect against backtracking
 		//   Old regex: ^(\d|[a-z]|[A-Z]|_)(\d|[a-z]|[A-Z]|-|_)*$
 		//   Right regex with atomic grouping: ^(? >\w[\w-]*)$ (no space between ? and >)
-		//   Working regex:
-		return('^(?=(\w[\w-]*))\1$');
+		//   Working regex: ^(?=(\w[\w-]*))\1$
+
+		$sPattern = MetaModel::GetModuleSetting(static::MODULE_CODE, static::FUNCTION_CODE, '');
+		if ($sPattern !== '') {
+			return '^'.$sPattern.'$';
+		}
+
+		return ('^(?=(\w[\w-]*))\1$');
 	}
 }
