784501ddb40251e444843928db542d0544012f5f
xtophe38 <christophe.naud@gmail.com>
2021-12-27 15:06:47 +0100

    Updates for 3.0

--- teemip-storage-mgmt-adaptor/module.teemip-storage-mgmt-adaptor.php
+++ teemip-storage-mgmt-adaptor/module.teemip-storage-mgmt-adaptor.php
@@ -23,7 +23,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__,
-	'teemip-storage-mgmt-adaptor/2.7.0',
+	'teemip-storage-mgmt-adaptor/3.0.0',
 	array(
 		// Identification
 		//
@@ -34,8 +34,8 @@ SetupWebPage::AddModule(
 		//
 		'dependencies' => array(
 			'itop-storage-mgmt/2.7.0',
-			'teemip-ip-mgmt/2.7.0',
-			'teemip-config-mgmt-adaptor/2.7.0'
+			'teemip-ip-mgmt/3.0.0',
+			'teemip-config-mgmt-adaptor/3.0.0',
 		),
 		'mandatory' => false,
 		'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
