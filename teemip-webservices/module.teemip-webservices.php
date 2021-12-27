b145cd8e5c9d165dce45bbe95d5c5364cbf066c7
xtophe38 <christophe.naud@gmail.com>
2021-12-27 12:45:53 +0100

    Updates for 3.0

--- teemip-webservices/module.teemip-webservices.php
+++ teemip-webservices/module.teemip-webservices.php
@@ -6,17 +6,17 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-webservices/2.7.1',
+	'teemip-webservices/3.0.0',
 	array(
 		// Identification
 		//
 		'label' => 'TeemIp WEB Services',
 		'category' => 'business',
-		
+
 		// Setup
 		//
 		'dependencies' => array(
-			'teemip-ip-mgmt/2.7.0',
+			'teemip-ip-mgmt/3.0.0',
 		),
 		'mandatory' => true,
 		'visible' => false,
