56ade34efb08210b20c91735078eb48653f780ad
xtophe38 <christophe.naud@gmail.com>
2021-12-28 12:22:49 +0100

    Update composer

---
+++ teemip-ip-mgmt/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '55468543d4bd8e7b0b463f8febc7ae188f4be71d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '55468543d4bd8e7b0b463f8febc7ae188f4be71d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
