00f24a592d75dda777b04dd340c50dc28fd4da43
xtophe38 <christophe.naud@gmail.com>
2022-06-07 21:38:26 +0200

    Correct some typos

--- teemip-network-mgmt-extended/en.dict.teemip-network-mgmt-extended.php
+++ teemip-network-mgmt-extended/en.dict.teemip-network-mgmt-extended.php
@@ -18,7 +18,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:InterfaceConnector+' => 'Connector used on network interfaces',
 	'Class:InterfaceConnector/Attribute:description' => 'Description',
 	'Class:InterfaceConnector/Attribute:description+' => '',
-	'Class:InterfaceConnector/Attribute:physicalinterfaces_list' => 'Physical Interfaces',
+	'Class:InterfaceConnector/Attribute:physicalinterfaces_list' => 'Physical interfaces',
 	'Class:InterfaceConnector/Attribute:physicalinterfaces_list+' => 'Physical interfaces with that connector',
 ));
 
@@ -31,7 +31,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:Layer2Protocol+' => 'Layer 2 protocol used on network interfaces',
 	'Class:Layer2Protocol/Attribute:description' => 'Description',
 	'Class:Layer2Protocol/Attribute:description+' => '',
-	'Class:Layer2Protocol/Attribute:ipinterfaces_list' => 'Network Interfaces',
+	'Class:Layer2Protocol/Attribute:ipinterfaces_list' => 'Network interfaces',
 	'Class:Layer2Protocol/Attribute:ipinterfaces_list+' => 'Network interfaces using that protocol',
 ));
 
@@ -44,7 +44,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:InterfaceSpeed+' => 'Speed set on network interfaces',
 	'Class:InterfaceSpeed/Attribute:description' => 'Description',
 	'Class:InterfaceSpeed/Attribute:description+' => '',
-	'Class:InterfaceSpeed/Attribute:ipinterfaces_list' => 'Network Interfaces',
+	'Class:InterfaceSpeed/Attribute:ipinterfaces_list' => 'Network interfaces',
 	'Class:InterfaceSpeed/Attribute:ipinterfaces_list+' => 'Network interfaces at that speed',
 ));
 
@@ -67,7 +67,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:ClusterNetwork' => 'Cluster Network',
 	'Class:ClusterNetwork+' => 'Hardware component for network device',
 	'Class:ClusterNetwork:baseinfo' => 'General Information',
-	'Class:ClusterNetwork:moreinfo' => 'More information',
+	'Class:ClusterNetwork:moreinfo' => 'More Information',
 	'Class:ClusterNetwork:Date' => 'Dates',
 	'Class:ClusterNetwork:otherinfo' => 'Other Information',
 	'Class:ClusterNetwork/Attribute:type' => 'Type',
@@ -85,7 +85,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:ClusterNetwork/Attribute:mode/Value:active_passive' => 'Active / Passive',
 	'Class:ClusterNetwork/Attribute:mode/Value:active_passive+' => '',
 	'Class:ClusterNetwork/Attribute:mode/Value:active_active' => 'Active / Active',
-    'Class:ClusterNetwork/Attribute:mode/Value:active_active+' => '',
+	'Class:ClusterNetwork/Attribute:mode/Value:active_active+' => '',
 	'Class:ClusterNetwork/Attribute:status' => 'Status',
 	'Class:ClusterNetwork/Attribute:status+' => '',
 	'Class:ClusterNetwork/Attribute:status/Value:production' => 'Production',
@@ -94,7 +94,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:ClusterNetwork/Attribute:status/Value:implementation+' => '',
 	'Class:ClusterNetwork/Attribute:networkdevices_list' => 'Nodes',
 	'Class:ClusterNetwork/Attribute:networkdevices_list+' => 'List of all network devices within the cluster',
-	'Class:ClusterNetwork/Attribute:ips_list' => 'IP Addresses',
+	'Class:ClusterNetwork/Attribute:ips_list' => 'IP addresses',
 	'Class:ClusterNetwork/Attribute:ips_list+' => 'List of all IP addresses used by the cluster',
 	'Class:ClusterNetwork/Attribute:redundancy' => 'High availability',
 	'Class:ClusterNetwork/Attribute:redundancy/disabled' => 'The cluster is up if all the network devices are up',
@@ -192,7 +192,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:AggregateLink/Attribute:description+' => '',
 	'Class:AggregateLink/Attribute:peer_id' => 'Peer aggregate',
 	'Class:AggregateLink/Attribute:peer_id+' => 'Aggregate link of the device at the other end of this link',
-	'Class:AggregateLink/Attribute:physicalinterfaces_list' => 'Physical Interfaces',
+	'Class:AggregateLink/Attribute:physicalinterfaces_list' => 'Physical interfaces',
 	'Class:AggregateLink/Attribute:physicalinterfaces_list+' => '',
 ));
 
