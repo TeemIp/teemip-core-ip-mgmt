1e1f32f29e032e0f601bff0ebfc4d0ae122ed9c8
xtophe38 <christophe.naud@gmail.com>
2023-03-10 14:06:47 +0100

    Correct parameter of DBObjectSearch and reformat file

--- teemip-framework/src/Model/_IPUsage.php
+++ teemip-framework/src/Model/_IPUsage.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * @copyright   Copyright (C) 2021 TeemIp
+ * @copyright   Copyright (C) 2023 TeemIp
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -20,21 +20,17 @@ class _IPUsage extends Typology
 	{
 		// Run standard iTop checks first
 		parent::DoCheckToWrite();
-		
+
 		// Only one NETWORK_IP_CODE, GATEWAY_IP_CODE and BROADCAST_IP_CODE can exist within an organization
 		$sName = $this->Get('name');
-		if (($sName == NETWORK_IP_CODE) || ($sName == GATEWAY_IP_CODE) || ($sName == BROADCAST_IP_CODE))
-		{
-			$sOrgId = $this->Get('org_id');
+		if (($sName == NETWORK_IP_CODE) || ($sName == GATEWAY_IP_CODE) || ($sName == BROADCAST_IP_CODE)) {
 			if ($this->IsNew()) {
-				$iKey = -1;
+				$sOQL = 'SELECT IPUsage AS u WHERE u.name = :name AND u.org_id = :org_id';
 			} else {
-				$iKey = $this->GetKey();
+				$sOQL = 'SELECT IPUsage AS u WHERE u.name = :name AND u.org_id = :org_id AND u.id != :id';
 			}
-			$sOQL = 'SELECT IPUsage AS u WHERE u.name = :name AND u.org_id = :org_id AND u.id != :key';
-			$oIpUsageSet = new CMDBObjectSet(DBObjectSearch::FromOQL($sOQL), array(), array('name' => $sName, 'org_id' => $sOrgId, 'id' => $iKey));
-			if ($oIpUsageSet->Count() > 0) {
-				// It's NOT a modification (keys are not the same), we deny the creation
+			$oIPUsageSet = new CMDBObjectSet(DBObjectSearch::FromOQL($sOQL), array(), array('name' => $sName, 'org_id' => $this->Get('org_id'), 'id' => $this->GetKey()));
+			if ($oIPUsageSet->CountExceeds(0)) {
 				$this->m_aCheckIssues[] = Dict::Format('UI:IPManagement:Action:New:IPUsage:AlreadyExists');
 			}
 		}
@@ -47,7 +43,7 @@ class _IPUsage extends Typology
 	{
 		$sFlagsFromParent = parent::GetAttributeFlags($sAttCode, $aReasons, $sTargetState);
 
-		if ((!$this->IsNew()) && (($sAttCode == 'name'))) {
+		if ($sAttCode == 'name') {
 			$sName = $this->Get('name');
 			if (($sName == NETWORK_IP_CODE) || ($sName == GATEWAY_IP_CODE) || ($sName == BROADCAST_IP_CODE)) {
 				return (OPT_ATT_READONLY | $sFlagsFromParent);
