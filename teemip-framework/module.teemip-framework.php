5a98a259f32da4494e158c41f8fcb047c06eb951
xtophe38 <christophe.naud@gmail.com>
2023-10-31 10:44:09 +0100

    Update module revision #

--- teemip-framework/module.teemip-framework.php
+++ teemip-framework/module.teemip-framework.php
@@ -6,7 +6,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-framework/3.1.1',
+	'teemip-framework/3.1.2',
 	array(
 		// Identification
 		//
