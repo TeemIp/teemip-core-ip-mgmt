7e17236917426ac3cce106382f57f9c74a03814d
xtophe38 <christophe.naud@gmail.com>
2023-11-03 21:37:04 +0100

    Remove specific code to handle versions older than 3.0

--- teemip-framework/src/Helper/DisplayMessage.php
+++ teemip-framework/src/Helper/DisplayMessage.php
@@ -15,14 +15,9 @@ class DisplayMessage {
 	 * @param $sMessage
 	 */
 	public static function Success(iTopWebPage $oP, $sMessage) {
-		if (version_compare(ITOP_DESIGN_LATEST_VERSION, '3.0', '<')) {
-			$sMessageContainer = "<div class=\"header_message message_ok teemip_message_status\">".$sMessage."</div>";
-			$oP->add($sMessageContainer);
-		} else {
-			$oPanel = PanelUIBlockFactory::MakeForSuccess('')
-				->AddHtml($sMessage);
-			$oP->AddUiBlock($oPanel);
-		}
+		$oPanel = PanelUIBlockFactory::MakeForSuccess('')
+			->AddHtml($sMessage);
+		$oP->AddUiBlock($oPanel);
 	}
 
 	/**
@@ -30,14 +25,9 @@ class DisplayMessage {
 	 * @param $sMessage
 	 */
 	public static function Info(iTopWebPage $oP, $sMessage) {
-		if (version_compare(ITOP_DESIGN_LATEST_VERSION, '3.0', '<')) {
-			$sMessageContainer = "<div class=\"header_message message_info teemip_message_status\">".$sMessage."</div>";
-			$oP->add($sMessageContainer);
-		} else {
-			$oPanel = PanelUIBlockFactory::MakeForInformation('')
-				->AddHtml($sMessage);
-			$oP->AddUiBlock($oPanel);
-		}
+		$oPanel = PanelUIBlockFactory::MakeForInformation('')
+			->AddHtml($sMessage);
+		$oP->AddUiBlock($oPanel);
 	}
 
 	/**
@@ -45,13 +35,8 @@ class DisplayMessage {
 	 * @param $sMessage
 	 */
 	public static function Warning(iTopWebPage $oP, $sMessage) {
-		if (version_compare(ITOP_DESIGN_LATEST_VERSION, '3.0', '<')) {
-			$sMessageContainer = "<div class=\"header_message message_error teemip_message_status\">".$sMessage."</div>";
-			$oP->add($sMessageContainer);
-		} else {
-			$oPanel = PanelUIBlockFactory::MakeForWarning('')
-				->AddHtml($sMessage);
-			$oP->AddUiBlock($oPanel);
-		}
+		$oPanel = PanelUIBlockFactory::MakeForWarning('')
+			->AddHtml($sMessage);
+		$oP->AddUiBlock($oPanel);
 	}
 }
